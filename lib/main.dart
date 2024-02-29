578a1b8edb017b0b3f97170fa6556a8d14a48957
Rajkumar Verma <rajkumarverma70790@gmail.com>
2024-02-29 10:01:29 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,7 @@ Future<void> main() async {
   try {
     await Firebase.initializeApp(
         options: const FirebaseOptions(
-            apiKey: "AIzaSyAHjC6mAe7-9V3_ugjJyZPfgxeaej1XRbc",
-            authDomain: "top108-97bfd.firebaseapp.com",
-            projectId: "top108-97bfd",
-            storageBucket: "top108-97bfd.appspot.com",
-            messagingSenderId: "842732551011",
-            appId: "1:842732551011:web:94edc8e3eb59e1665d3156",
-            measurementId: "G-5HJS5N7BN7"));
+           ));
     runApp(const MyApp());
   } catch (e) {
     debugPrint(e.toString());
